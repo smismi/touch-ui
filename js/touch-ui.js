66c8bc00e37e84af76aaf00a18231b10abf1cbab
smismi <smirnov.ru@gmail.com>
2012-08-09 23:11:10 +0400

    all over

--- js/touch-ui.js
+++ js/touch-ui.js
@@ -7,15 +7,125 @@
  */
 
 (function(){
+    var m = Math,
+        mround = function (r) { return r >> 0; },
+        vendor = (/webkit/i).test(navigator.appVersion) ? 'webkit' :
+            (/firefox/i).test(navigator.userAgent) ? 'Moz' :
+                'opera' in window ? 'O' : '',
+
+    // Browser capabilities
+        isAndroid = (/android/gi).test(navigator.appVersion),
+        isIDevice = (/iphone|ipad/gi).test(navigator.appVersion),
+        isPlaybook = (/playbook/gi).test(navigator.appVersion),
+        isTouchPad = (/hp-tablet/gi).test(navigator.appVersion),
+
+        has3d = 'WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix(),
+        hasTouch = 'ontouchstart' in window && !isTouchPad,
+        hasTransform = vendor + 'Transform' in document.documentElement.style,
+        hasTransitionEnd = isIDevice || isPlaybook,
+
+        nextFrame = (function() {
+            return window.requestAnimationFrame
+                || window.webkitRequestAnimationFrame
+                || window.mozRequestAnimationFrame
+                || window.oRequestAnimationFrame
+                || window.msRequestAnimationFrame
+                || function(callback) { return setTimeout(callback, 17); }
+        })(),
+        cancelFrame = (function () {
+            return window.cancelRequestAnimationFrame
+                || window.webkitCancelAnimationFrame
+                || window.webkitCancelRequestAnimationFrame
+                || window.mozCancelRequestAnimationFrame
+                || window.oCancelRequestAnimationFrame
+                || window.msCancelRequestAnimationFrame
+                || clearTimeout
+        })(),
+
+    // Events
+        RESIZE_EV = 'onorientationchange' in window ? 'orientationchange' : 'resize',
+        START_EV = hasTouch ? 'touchstart' : 'mousedown',
+        MOVE_EV = hasTouch ? 'touchmove' : 'mousemove',
+        END_EV = hasTouch ? 'touchend' : 'mouseup',
+        CANCEL_EV = hasTouch ? 'touchcancel' : 'mouseup',
+
+    // Helpers
+        trnOpen = 'translate' + (has3d ? '3d(' : '('),
+        trnClose = has3d ? ',0)' : ')',
 // Constructor
     TouchUI = function (el, options) {
-        alert("go again")
+        var that = this,
+            doc = document,
+            i;
+        that.wrapper = typeof el == 'object' ? el : doc.getElementById(el);
+        that.wrapper.style.border = '1px solid red';
+
+
+
+        // Default options
+        that.options = {
+            hScroll: true,
+            vScroll: true,
+            x: 0,
+            y: 0,
+            bounce: true,
+            bounceLock: false,
+            momentum: true,
+            lockDirection: true,
+            useTransform: true,
+            useTransition: false,
+
+            // Events
+            onClick: null,
+            onRefresh: null,
+            onBeforeScrollStart: function (e) { e.preventDefault(); },
+            onScrollStart: null,
+            onBeforeScrollMove: null,
+            onScrollMove: null,
+            onBeforeScrollEnd: null,
+            onScrollEnd: null,
+            onTouchEnd: null,
+            onDestroy: null
+        };
+
+
+        that._bind(START_EV, window);
+
     };
 // Prototype
     TouchUI.prototype = {
         enabled: true,
         x: 0,
-        y: 0
+        y: 0,
+        handleEvent: function (e) {
+            var that = this;
+            switch(e.type) {
+                case START_EV:
+                    if (!hasTouch && e.button !== 0) return;
+                    that._start(e);
+                    break;
+            }
+        },
+        _start: function (e) {
+            var that = this,
+                point = hasTouch ? e.touches[0] : e,
+                matrix, x, y;
+           console.log(point.type)
+//            that._bind(MOVE_EV);
+//            that._bind(END_EV);
+//            that._bind(CANCEL_EV);
+        },
+
+        _bind: function (type, el, bubble) {
+            el.addEventListener(type, this, !!bubble);
+        },
+
+        _unbind: function (type, el, bubble) {
+            el.removeEventListener(type, this, !!bubble);
+        }
     }
+
+    if (typeof exports !== 'undefined') exports.TouchUI = TouchUI;
+    else window.TouchUI = TouchUI;
 })();
 
