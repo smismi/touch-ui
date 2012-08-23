6c76af71cb737ba32bcabcea74980544c6339e4d
smismi <smirnov.ru@gmail.com>
2012-08-23 22:57:26 +0400

    vsyakoe

--- js/scroll.js
+++ js/scroll.js
@@ -15,6 +15,14 @@ function $px(x) {
 
 (function () {
 
+    //touch extention
+    var isAndroid = (/android/gi).test(navigator.appVersion);
+    var isIDevice = (/iphone|ipad/gi).test(navigator.appVersion);
+    var isPlaybook = (/playbook/gi).test(navigator.appVersion);
+    var isTouchPad = (/hp-tablet/gi).test(navigator.appVersion);
+    var isTouch = 'ontouchstart' in window && !isTouchPad;
+
+    var START_EV = isTouch ? 'touchstart' : 'mousedown';
 
     Scroll = function(el, options) {
         var that = this;
@@ -29,16 +37,53 @@ function $px(x) {
                 x: 0,
                 y: 0
             }
-        }
+        };
         // User defined options
         for (i in options) that.options[i] = options[i];
 
         that.drawScroller(that.scroller);
         that.activateDisabler();
-    },
+        that._bind(START_EV, that.scroller);
+//        if (isTouch) {
+//            that.bindTouch(that.scroller);
+//        }
+    };
 
     Scroll.prototype = {
-
+//    touch
+        handleEvent: function (e) {
+                var that = this;
+                switch(e.type) {
+                case START_EV:
+                    alert(1);
+                    if (!hasTouch && e.button !== 0) return;
+                    that._start(e);
+                    break;
+                case MOVE_EV: that._move(e); break;
+                case END_EV:
+                case CANCEL_EV: that._end(e); break;
+                case RESIZE_EV: that._resize(); break;
+                case 'mouseout': that._mouseout(e); break;
+                case 'webkitTransitionEnd': that._transitionEnd(e); break;
+            }
+        },
+//        bindTouch: function(el) {
+//            var that = this;
+//            elem = that.wrapper;
+//            elem.addEventListener((isTouch ? "touchstart" : "mousedown"), that.onTouchStart, false);
+//
+//        },
+//        onTouchStart:function (e) {
+//            var that = this;
+//
+//            if (!that.options.enabled) return;
+////                point = e.touches[0];
+//            point = isTouch ? e.touches[0] : e;
+//            that._log(point.clientX);
+//            that._log(point.clientY);
+//            // отменить действие по умолчанию (прокрутку элемента/страницы)
+//            e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+//        },
 //    utils
         drawScroller : function(el) {
             var that = this;
@@ -145,7 +190,13 @@ function $px(x) {
             var logs = document.createElement("div");
             logs.innerHTML = text;
             document.getElementById("log").appendChild(logs);
-        }
+        },
+        _bind: function (type, el, bubble) {
+            el.addEventListener(type, this, !!bubble);
+        },
 
+        _unbind: function (type, el, bubble) {
+            el.removeEventListener(type, this, !!bubble);
+        }
     }
 })();
